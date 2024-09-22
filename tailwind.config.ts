e0b215ea5000c30b37fcccd3db9273c2cf242aa1
moeko-iwai <moepyxxxxxx@gmail.com>
2024-09-22 12:27:58 +0900

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        card: "var(--card)",
+        black: "var(--black)",
+        white: "var(--white)",
       },
     },
   },
